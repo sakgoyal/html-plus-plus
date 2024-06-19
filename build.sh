e0ed96c6b0d1c28f19864c21e49b6b3a45dc2cda
Saksham Goyal <sakgoy2001@gmail.com>
2024-06-19 02:43:47 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-compiler=g++-9
-standard=-"-std=c++2a"
 flags="-Wall -Wextra -pedantic-errors"
 
-$compiler $standard $flags $@ -o example-all-elements example-all-elements.cpp
-$compiler $standard $flags $@ -o example-readme-1 example-readme-1.cpp
+g++ -std=c++23 $flags $@ -o example-all-elements example-all-elements.cpp
+g++ -std=c++23 $flags $@ -o example-readme-1 example-readme-1.cpp
